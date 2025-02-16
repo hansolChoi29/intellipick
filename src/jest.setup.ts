2611b7db4ee05af91e0634c25581f7945d2f5043
hansolChoi29 <ggksthf29@gmail.com>
2025-02-16 18:59:52 +0900

    chore:jest보고서작성완료

--- src/jest.setup.ts
+++ src/jest.setup.ts
@@ -1,2 +1,6 @@
-import "@testing-library/jest-dom/extend-expect"; // jest-dom matcher 설정
-import "@testing-library/jest-dom";
+// jest.setup.ts
+import { TextEncoder as TE, TextDecoder as TD } from "util";
+
+// global 객체에 TextEncoder와 TextDecoder 설정 (타입을 명확히 지정)
+(global as unknown as { TextEncoder: typeof TE }).TextEncoder = TE;
+(global as unknown as { TextDecoder: typeof TD }).TextDecoder = TD;
